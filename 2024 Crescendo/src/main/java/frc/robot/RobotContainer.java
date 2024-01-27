eb3c6d5ac96c841e0be3b489dad76e65acb637fe
James <jfriggs1877@gmail.com>
2024-01-27 18:31:03 -0500

    Finished the retract arm command. IDK why it wasn't finished?

--- 2024 Crescendo/src/main/java/frc/robot/RobotContainer.java
+++ 2024 Crescendo/src/main/java/frc/robot/RobotContainer.java
@@ -7,8 +7,6 @@ package frc.robot;
 //WPILIB
 import edu.wpi.first.wpilibj2.command.button.CommandJoystick;
 import edu.wpi.first.wpilibj2.command.Command;
-import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
-import edu.wpi.first.wpilibj2.command.button.Trigger;
 import edu.wpi.first.wpilibj.XboxController;
 
 
