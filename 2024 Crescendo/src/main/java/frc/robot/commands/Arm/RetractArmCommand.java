a27060b066553d5bf6925904ccb77fc7de23b083
Logan <woodyandbuzzman@gmail.com>
2024-01-27 18:25:14 -0500

    ADDED RED AND BLUE LED OPTIONS

--- 2024 Crescendo/src/main/java/frc/robot/commands/Arm/RetractArmCommand.java
+++ 2024 Crescendo/src/main/java/frc/robot/commands/Arm/RetractArmCommand.java
@@ -1,36 +1,7 @@
 package frc.robot.commands.Arm;
-import edu.wpi.first.wpilibj2.command.Command;
-import frc.robot.subsystems.ArmSubsystem;
 
-
-public class RetractArmCommand extends Command{
-
-    ArmSubsystem armSubsystem;
+public class RetreactArmCommand {
     
-    public RetractArmCommand(ArmSubsystem armSubsystem){
-        
-        this.armSubsystem = armSubsystem;
-        addRequirements(armSubsystem);
+    
 
-    }
-
-    @Override
-    public void initialize(){
-        
-        armSubsystem.retractArm();
-
-    }
-
-    @Override
-    public void execute() {}
-
-    @Override
-    public void end(boolean interrupted) {
-        
-    }
-
-    @Override
-    public boolean isFinished() {
-        return false;
-    }
 }
