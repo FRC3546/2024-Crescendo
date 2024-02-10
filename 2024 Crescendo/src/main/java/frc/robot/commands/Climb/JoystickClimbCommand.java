37fad5370357e28be44dc569cefe941cf13303cf
James <jfriggs1877@gmail.com>
2024-02-10 18:15:43 -0500

    Created a climb command for controlling the climber

--- 2024 Crescendo/src/main/java/frc/robot/commands/Climb/JoystickClimbCommand.java
+++ 2024 Crescendo/src/main/java/frc/robot/commands/Climb/JoystickClimbCommand.java
@@ -3,12 +3,12 @@ package frc.robot.commands.Climb;
 import edu.wpi.first.wpilibj2.command.Command;
 import frc.robot.subsystems.ClimbSubsystem;
 
-public class ClimbCommand extends Command {
+public class JoystickClimbCommand extends Command {
 
     private final ClimbSubsystem climbSubsystem;
     private double speed;
 
-    public ClimbCommand(ClimbSubsystem climbSubsystem, double speed) {
+    public JoystickClimbCommand(ClimbSubsystem climbSubsystem, double speed) {
 
         this.climbSubsystem = climbSubsystem;
         this.speed = speed;
@@ -17,13 +17,12 @@ public class ClimbCommand extends Command {
     }
 
     @Override
-    public void initialize() {
-        
-        climbSubsystem.rotateClimbers(speed);
-    }
+    public void initialize() {}
 
     @Override
-    public void execute() {}
+    public void execute() {
+        climbSubsystem.rotateClimbers(speed);
+    }
 
     @Override
     public void end(boolean interrupted) {
