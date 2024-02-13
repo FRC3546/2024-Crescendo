8612e8d4f78bbc4ad06c37c58c8af298321ed016
Logan <woodyandbuzzman@gmail.com>
2024-02-12 20:05:08 -0500

    Addded Led command

---
+++ 2024 Crescendo/src/main/java/frc/robot/commands/Intake/LedCarryingNoteCommand.java
@@ -0,0 +1,9 @@
+package frc.robot.commands.Leds;
+import frc.robot.subsystems.LedSubsystem;
+import edu.wpi.first.wpilibj2.command.Command;
+
+public class LedCarryingNoteCommand extends Command{
+
+    private final LedSubsystem ledSubsystem;
+    
+}
--- 2024 Crescendo/src/main/java/frc/robot/commands/Leds/LedCarryingNoteCommand.java
+++ 2024 Crescendo/src/main/java/frc/robot/commands/Leds/LedCarryingNoteCommand.java
@@ -1,5 +1,43 @@
 package frc.robot.commands.Leds;
-
-
-
+import frc.robot.subsystems.IntakeShooterSubsystem;
 import frc.robot.subsystems.LedSubsystem;
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.WaitCommand;
+
+public class LedCarryingNoteCommand extends Command{
+
+    private final LedSubsystem ledSubsystem;
+
+    public LedCarryingNoteCommand(LedSubsystem ledSubsystem){
+       
+        this.ledSubsystem = ledSubsystem;
+
+        addRequirements(ledSubsystem);
+
+    }
+
+     // Called when the command is initially scheduled.
+  @Override
+  public void initialize() {}
+
+  // Called every time the scheduler runs while the command is scheduled.
+  @Override
+  public void execute() {
+    
+    ledSubsystem.LEDOrange();
+    new WaitCommand(0.5);
+    ledSubsystem.LEDOff();
+    new WaitCommand(0.5)
+    
+  }
+
+  // Called once the command ends or is interrupted.
+  @Override
+  public void end(boolean interrupted) {}
+
+  // Returns true when the command should end.
+  @Override
+  public boolean isFinished() {
+    return false;
+  }
+}
